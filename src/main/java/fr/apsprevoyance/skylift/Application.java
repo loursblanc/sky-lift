eee761930e00f344b2c66dde8e3758a8be799073
Alexandre Bourse <alexandre.bourse@gmail.com>
2025-02-24 10:24:23 +0100

    Setup Vaadim

--- src/main/java/fr/apsprevoyance/skylift/Application.java
+++ src/main/java/fr/apsprevoyance/skylift/Application.java
@@ -1,9 +1,10 @@
 package fr.apsprevoyance.skylift;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
